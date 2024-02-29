74887b6d6f3a24c16ad3548881253f3edc8810ec
Guariko <creatingsomething2021@gmail.com>
2024-02-29 20:55:31 -0300

    Hero complete

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,9 +1,40 @@
+import { Link } from "react-router-dom";
+import heroImage from "../../assets/images/hero.png";
+
 export default function Hero() {
   return (
-    <section>
-      <article>
-        <h1>Hello world</h1>
-      </article>
+    <section className="bg-regular-gradient">
+      <div className="text-white font-bold p-3 py-16 flex flex-col gap-10 md:flex-row md:gap-16 md:p-4 md:py-28 md:items-center regular-max-w m-auto">
+        <article className="flex flex-col gap-7">
+          <h1 className="text-2xl max-w-[26ch] lg:text-4xl lg:max-w-[30ch]">
+            Bem-vindo à Conferência Estadual de Ciência, Tecnologia e Inovação!
+          </h1>
+
+          <p className="font-normal text-base max-w-[52ch]">
+            Descubra o futuro da pesquisa e inovação enquanto reunimos
+            especialistas, acadêmicos e inovadores sob o mesmo teto. Junte-se a
+            nós enquanto exploramos avanços inovadores e traçamos o curso para
+            um futuro mais promissor.
+          </p>
+
+          <div className="flex gap-7 flex-wrap md:flex-nowrap md:flex-row md:items-center">
+            <Link to="inscrever-se" className="button">
+              Inscrever-se
+            </Link>
+            <a href="#" className="button">
+              Saber mais
+            </a>
+          </div>
+        </article>
+
+        <picture className="self-center">
+          <img
+            src={heroImage}
+            alt="Jovens usando técnologia"
+            className="w-full max-w-[34rem] md:max-w-[44rem] md:object-cover"
+          />
+        </picture>
+      </div>
     </section>
   );
 }
