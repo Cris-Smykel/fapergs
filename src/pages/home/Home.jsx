23ed35403e4afc5e1fe920dda869c6514d44fcd7
Guariko <creatingsomething2021@gmail.com>
2024-03-01 01:32:07 -0300

    Testimonial section complete

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import Hero from "../../components/home/Hero";
 import About from "../../components/home/About";
+import Testimonial from "../../components/home/Testimonial";
 
 export default function Home() {
   return (
@@ -7,6 +8,8 @@ export default function Home() {
       <Hero />
 
       <About />
+
+      <Testimonial />
     </main>
   );
 }
